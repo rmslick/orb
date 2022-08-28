e8a24fce707292e578246cde080d4cb88f3eb433
rmslick <rmslick@buffalo.edu>
2022-08-28 12:43:31 -0400

    Added in noise injection into velocity, tested. Adding noise into height

--- CubeSat.py
+++ CubeSat.py
@@ -38,7 +38,7 @@ class SingleAxisCubeSat:
         # Pitch transition matrix
         self.A = A
         self.torque_transition = np.array([0,1/self.J])
-
+        self.cube_dims = [1,1,1,1] # meters
         '''
             Orbital Dynamics: Position properties - Angle of elevation and pitch angle
         '''
@@ -65,18 +65,19 @@ class SingleAxisCubeSat:
         self.p = 2.8*(10**(-1*12))
         # Drag coefficient
         self.C_d = 2.5
-        # Velocity
+        # Velocity - velocity (meters/min?)
         self.V = None
         # Dimensional constant
-        self.summa_R_A = None
-        # gravitational constant of eaarth (9.807 m/s²)
-        self.u = 9.807
+        self.summa_R_A = sum(self.cube_dims)
+        # gravitational constant of eaarth (9.807*60*60 m/min²)
+        self.u = 35316
+        self.t_atmospheric = None
     # Equations of angle
     def getSingleAxisMotion(self,ang_rate, ang_vel, torque_command, disturbance_torque):
         return self.A * np.array([ang_rate,ang_vel])+self.torque_transition*torque_command+ self.torque_transition * disturbance_torque
     # Equations of position
-    def getAngleOfElevationROC(self):
-        return ( (self.Re+self.h)*sin(self.e+ self.phi) * self.orbit.o_dot ) / (self.Re*sin(self.e) - (self.Re+self.h)*sin(self.e+self.phi) ) 
+    def getAngleOfElevationROC(self,angular_val):
+        return ( (self.Re+self.h)*sin(self.e+ self.phi) * angular_val ) / (self.Re*sin(self.e) - (self.Re+self.h)*sin(self.e+self.phi) ) 
     def getAngleOfElevation(self):
         return atan( ( -1*self.Re+ (self.Re+self.h) * cos(self.phi) ) ) / ((self.Re +self.h)*sin( self.phi )) 
     '''
@@ -84,32 +85,61 @@ class SingleAxisCubeSat:
         return asin(self.Re/(self.Re+self.h)*cos(self.e))
     def getPosition(self):
         return self.getAngleOfElevation(), self.getPitchAngle()
+    '''
     # Equations of Drag
     def getAtmosphericDrag(self):
-        return -1*0.5*self.C_d*self.p*self.V**2*self.summa_R_A
+        return -1*0.5*self.C_d*self.p*(self.V**2)*self.summa_R_A
     def getMagneticDisturbance(self):
         pass
     def getGravityGradient(self, R):
         return (3*self.u/R**3)(R*self.J*R)
     '''
-    def simulate(self,start_time,end_time,interval):
+        interval - Time between runs (minutes)
+    '''
+    def simulate(self,start_time,end_time,interval,orb_rad):
         # change angle between satellite and ground station
         t = start_time
         all_positions = []
+        # inject noise into angular velocity
+        p = np.array([self.orbit.o_dot for i in range(0,end_time - start_time+1) ])
+        height = np.array([self.h for i in range(0,end_time - start_time+1) ])
+        # Conjecture: Torque forces cause velocity to deviate by +/- 0.0001 radians per minute
+        n = np.random.normal(0, .0001, end_time - start_time +1) 
+        # Conjecture: Torque forces cause height to deviate by +/- 0.0001
+        n_h = np.random.normal(0, .00001, end_time - start_time +1)
+        pn = p+n
+        jn = height + n_h
+        
+        
+        accessor= 0
         while t <= end_time:
-            self.phi = t*self.orbit.o_dot
+            # inject slight noise into height
+            # radians per minute
+            self.phi = t*pn[accessor]
+            # s - subtended angle angle between instances
+            # https://math.libretexts.org/Bookshelves/Precalculus/Book%3A_Trigonometry_(Sundstrom_and_Schlicker)/01%3A_The_Trigonometric_Functions/1.04%3A_Velocity_and_Angular_Velocity
+            theta = interval*pn[accessor]
+            r = orb_rad
+            # velocity (meters/min?)
+            self.V = r*theta
             
             if self.phi > 2*pi:
                 self.phi -= 2*pi
             
             # set angle of elevation
             self.e = self.getAngleOfElevation()
-            self.e_dot = self.getAngleOfElevationROC()
-            #self.pitch = asin((self.Re+(self.Re+self.h)*cos(self.e)))
+            self.e_dot = self.getAngleOfElevationROC(pn[accessor])
+            self.pitch = asin( (self.Re/(self.Re+self.h))* cos(self.e) )
+            print(self.e)
+            op = OrbitalPosition(self.phi, self.e, self.e_dot)
             
-            #op = OrbitalPosition(self.phi, self.e, self.e_dot)
-            all_positions.append(self.phi)
+            '''
+                Drag calculations
+            '''
+
+            all_positions.append(op)
             t+= interval
+            accessor += 1
         return all_positions
 
 
@@ -117,6 +147,7 @@ class SingleAxisCubeSat:
 o = Orbit(100, 6371000+400*1000 , 6371000+400*1000, 6371000+400*1000)
 print(o.o_dot)
 s = SingleAxisCubeSat(5,o)
-pos = s.simulate(1,100,1)
+pos = s.simulate(1,100,1,6371000+400*1000)
+pos = [p.phi for p in pos]
 #show_orbit(1,np.linspace(0,2*np.pi,360, endpoint=False))
 show_orbit(6371000+400*1000,pos,6371000+1992750)
